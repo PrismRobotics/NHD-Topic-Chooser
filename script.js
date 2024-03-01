68d14167e69f47c738114e1d669461985bb45d5c
PrismRobotics <161392373+PrismRobotics@users.noreply.github.com>
2024-03-01 17:12:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -13,21 +13,6 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 // Access your API key (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(API_KEY);
 
-async function run() {
-  // For text-only input, use the gemini-pro model
-  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-
-  const prompt = "Please take the folowing questions and responses about a persons intrests and return 3 topics and their brief overviews that you would recomend for them in National History Day. Do not say anything else. To the question 'Choose a person in history you find inspiring or fantastic' they respnded, '" + Q1
-
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  const element = document.getElementById("AiResponse");
-  element.innerHTML = text;
-  console.log(text);
-}
-
-
 
 function validateForms() {
   var Q1 = document.forms["form1"]["Q1"].value;
